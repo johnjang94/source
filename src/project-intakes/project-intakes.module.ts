82adf871570a26e397a5852f97e2dcbf73137271
John Jang <jscolbe9675@gmail.com>
2026-03-12 15:06:59 -0400

    debugging for project

--- src/project-intakes/project-intakes.module.ts
+++ src/project-intakes/project-intakes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectIntakesController } from './project-intakes.controller';
 import { ProjectIntakesService } from './project-intakes.service';
 import { PrismaModule } from '../prisma/prisma.module';
-import { StorageService } from 'src/r2/storage.service';
+import { R2Module } from '../r2/r2.module';
+import { StorageService } from '../r2/storage.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, R2Module],
   controllers: [ProjectIntakesController],
   providers: [ProjectIntakesService, StorageService],
 })
